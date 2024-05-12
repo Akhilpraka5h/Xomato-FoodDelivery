7a370d64250189589135c62c8d844c39ca85fa87
Akhilpraka5h <134758922+Akhilpraka5h@users.noreply.github.com>
2024-05-12 19:32:44 +0530

    update amount details

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -25,7 +25,7 @@ const placeOrder = async (req, res) => {
           name: item.name,
         },
         // unit_amount: item.price * 100 * 80,
-        unit_amount: item.price,
+        unit_amount: item.price * 100,
       },
       quantity: item.quantity,
     }));
@@ -35,7 +35,7 @@ const placeOrder = async (req, res) => {
         currency: "inr",
         product_data: { name: "Delivery Charges" },
         // unit_amount: 2 * 100 * 80,
-        unit_amount: 20,
+        unit_amount: 20 * 100,
       },
       quantity: 1,
     });
