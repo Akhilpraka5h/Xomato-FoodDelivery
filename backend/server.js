1ffcc985c07c287b4040cdab4a8f9132b2e1eba0
Akhilpraka5h <134758922+Akhilpraka5h@users.noreply.github.com>
2024-06-25 08:39:57 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ import orderRouter from "./routes/orderRoute.js";
 //intialize
 const app = express();
 //define port where server is running
-const port = 4000;
+const port = process.env.PORT || 4000;
 //middleware init
 app.use(express.json());
 //asses bacnd frm frntend
